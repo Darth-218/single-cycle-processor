7eb4d56772b7be04cfc782d2a2880b2087f69205
OmarHesham2356 <o.hesham2356@nu.edu.eg>
2025-12-18 22:17:11 +0200

    added display part to see the output

--- register-file/register_file_test.v
+++ register-file/register_file_test.v
@@ -25,7 +25,9 @@ module tb_register_file;
     always #5 clk = ~clk;
 
     initial begin
-        // Initialize
+      	$display("Time\tRegWrite\trd\trs1\trs2\twriteData\treadData1\treadData2");
+        
+      	// Initialize
         clk = 0;
         RegWrite = 0;
         rs1 = 0;
@@ -34,40 +36,54 @@ module tb_register_file;
         writeData = 0;
 
         #10;
+		
+
 
         // Test 1: Write 10 to x5
         RegWrite = 1;
         rd = 5;
         writeData = 64'd10;
         #10;
+		$display("%0t\t%b\t\t%0d\t%0d\t%0d\t%0d\t\t-\t\t-",
+                 $time, RegWrite, rd, rs1, rs2, writeData);
 
         // Read x5
         RegWrite = 0;
         rs1 = 5;
         #5;
+		$display("%0t\t%b\t\t-\t%0d\t-\t-\t\t%0d\t\t-  (Expected: 10)",
+         		$time, RegWrite, rs1, readData1);
 
         // Test 2: Write 25 to x3
         RegWrite = 1;
         rd = 3;
         writeData = 64'd25;
         #10;
+		$display("%0t\t%b\t\t%0d\t-\t-\t%0d\t\t-\t\t-",
+         		$time, RegWrite, rd, writeData);
 
         // Read x3 and x5
         RegWrite = 0;
         rs1 = 3;
         rs2 = 5;
         #5;
+		$display("%0t\t%b\t\t-\t%0d\t%0d\t-\t\t%0d\t\t%0d  (Expected: 25, 10)",
+         		$time, RegWrite, rs1, rs2, readData1, readData2);
 
         // Test 3: Try to write to x0
         RegWrite = 1;
         rd = 0;
         writeData = 64'd99;
         #10;
+		$display("%0t\t%b\t\t%0d\t-\t-\t%0d\t\t-\t\t-  (Write to x0 ignored)",
+                 $time, RegWrite, rd, writeData);
 
         // Read x0
         RegWrite = 0;
         rs1 = 0;
         #5;
+		$display("%0t\t%b\t\t-\t%0d\t-\t-\t\t%0d\t\t-  (Expected: 0)",
+         $time, RegWrite, rs1, readData1);
 
         $finish;
     end
