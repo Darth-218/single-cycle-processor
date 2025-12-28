9988f9ff1d0bd7a3dcce1f117dd832fba2b462be
Yahia Gaber <yahia.hany.gaber@proton.me>
2025-12-28 23:23:31 +0200

    more stuff

--- single_cycle_processor/single_cycle_processor.v
+++ single_cycle_processor/single_cycle_processor.v
@@ -4,28 +4,32 @@ module single_cycle_processor (
 );
 
   wire [63:0] pc;
-  wire [1:0] pc_src;
-  wire branch;
-  wire [63:0] imm;
-  wire [2:0] imm_type;
+  wire [63:0] pc_plus4;
+  assign pc_plus4 = pc + 64'd4;
 
   wire [31:0] instruction;
-  wire [6:0] opcode = instruction[6:0];
-  wire [2:0] funct3 = instruction[14:12];
-  wire funct7 = instruction[30];
+  wire [ 6:0] opcode = instruction[6:0];
+  wire [ 2:0] funct3 = instruction[14:12];
+  wire        funct7 = instruction[30];
 
-  wire [4:0] rs1_address = instruction[19:15];
-  wire [4:0] rs2_address = instruction[24:20];
-  wire [4:0] rd_address = instruction[11:7];
+  wire [ 4:0] rs1_address = instruction[19:15];
+  wire [ 4:0] rs2_address = instruction[24:20];
+  wire [ 4:0] rd_address = instruction[11:7];
+
+  wire reg_write, mem_read, mem_write, mem_to_reg;
+  wire branch, alu_src;
+  wire [1:0] alu_op;
+  wire [1:0] pc_src;
+  wire [2:0] imm_type;
 
   wire [63:0] rs1_data, rs2_data;
-
-  wire reg_write, mem_read, mem_write, mem_to_reg, alu_src;
-
-  wire [1:0] alu_op;
-  wire [3:0] alu_ctl;
+  wire [63:0] imm;
+  wire [63:0] alu_in2;
   wire [63:0] alu_out;
-  wire zero;
+  wire [63:0] mem_data;
+  wire [63:0] write_data;
+  wire [ 3:0] alu_ctl;
+  wire        zero;
 
   program_counter PC (
       .clock(clock),
@@ -47,18 +51,19 @@ module single_cycle_processor (
       .rs1_address(rs1_address),
       .rs2_address(rs2_address),
       .rd_address(rd_address),
-      .write_data(alu_out),
+      .write_data(write_data),
       .rs1_data(rs1_data),
       .rs2_data(rs2_data)
   );
 
   immediate_generator IG (
       .instruction(instruction),
+      .imm_type(imm_type),
       .imm(imm)
   );
 
   control_unit CU (
-      .opcode(instruction[6:0]),
+      .opcode(opcode),
       .reg_write(reg_write),
       .mem_read(mem_read),
       .mem_write(mem_write),
@@ -72,14 +77,16 @@ module single_cycle_processor (
 
   alu_control AC (
       .alu_op  (alu_op),
-      .alu_ctrl(alu_ctl),
       .funct3  (funct3),
-      .funct7  (funct7)
+      .funct7_5(funct7),
+      .alu_ctrl(alu_ctl)
   );
+
+  assign alu_in2 = alu_src ? imm : rs2_data;
 
   alu ALU (
       .rs1_data(rs1_data),
-      .rs2_data(rs2_data),
+      .rs2_data(alu_in2),
       .alu_ctrl(alu_ctl),
       .result(alu_out),
       .zero(zero)
@@ -89,9 +96,12 @@ module single_cycle_processor (
       .clock(clock),
       .mem_write(mem_write),
       .mem_read(mem_read),
-      .address(rs2_data),
-      .write_data(alu_out),
-      .read_data(alu_out)
+      .address(alu_out),
+      .write_data(rs2_data),
+      .read_data(mem_data)
   );
 
+  assign write_data = mem_to_reg ? mem_data : (pc_src == 2'b10) ? pc_plus4 : alu_out;
+
 endmodule
+
