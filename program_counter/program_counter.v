9c9751048320819fe30b7f24f2cbe179dcf4b788
Yahia Gaber <yahia.hany.gaber@proton.me>
2025-12-29 02:23:51 +0200

    fix: Immediate value shifting

--- program_counter/program_counter.v
+++ program_counter/program_counter.v
@@ -1,7 +1,7 @@
 module program_counter (
     input  wire        clock,
     input  wire        reset,
-    input  wire [1:0]  pc_src,
+    input  wire [ 1:0] pc_src,
     input  wire        zero,
     input  wire [63:0] imm,
     input  wire [63:0] alu_result,
@@ -13,8 +13,8 @@ module program_counter (
   wire [63:0] pc_next;
   wire        branch_taken;
 
-  assign pc_plus4     = pc_current + 64'd4;
-  assign pc_branch    = pc_current + imm;
+  assign pc_plus4 = pc_current + 64'd4;
+  assign pc_branch = pc_current + (imm >> 1);
   assign branch_taken = (pc_src == 2'b01) && zero;
 
   assign pc_next = (pc_src == 2'b01 && branch_taken) ? pc_branch :
