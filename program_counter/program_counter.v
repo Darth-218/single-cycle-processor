5fca409e583727ae022bdcbb34941f6e13627cf2
Yahia Gaber <yahia.hany.gaber@proton.me>
2025-12-29 02:24:21 +0200

    fix: Shift direction

--- program_counter/program_counter.v
+++ program_counter/program_counter.v
@@ -14,7 +14,7 @@ module program_counter (
   wire        branch_taken;
 
   assign pc_plus4 = pc_current + 64'd4;
-  assign pc_branch = pc_current + (imm >> 1);
+  assign pc_branch = pc_current + (imm << 1);
   assign branch_taken = (pc_src == 2'b01) && zero;
 
   assign pc_next = (pc_src == 2'b01 && branch_taken) ? pc_branch :
